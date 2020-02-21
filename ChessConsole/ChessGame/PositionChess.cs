e072a30bdd9e9912f06c12721739f2d5ef3daa79
MayconEQ <m_equeiroz@hotmail.com>
2020-02-20 22:34:53 -0300

    converting position char number in number number to use in matrix

---
+++ ChessConsole/ChessGame/PositionChess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chessboard;
+
+namespace ChessGame {
+    class PositionChess {
+        public char Col { get; set; }
+        public int Row { get; set; }
+        public PositionChess(char col, int row) {
+            Col = col;
+            Row = row;
+        }
+        public Position ToPosition() {
+            return new Position(8 - Row, Col - 'a');
+        }
+        public override string ToString() {
+            return Col + Row.ToString();
+        }
+    }
+}
